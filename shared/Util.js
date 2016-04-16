46aaf91598c8ac1eba53b0a37c10deccad2ce832
omgimanerd <alvin.lin.dev@gmail.com>
2016-04-16 15:26:28 -0400

    server side gulp lint errors fixed

--- shared/Util.js
+++ shared/Util.js
@@ -87,8 +87,8 @@ Util.getManhattanDistance = function(x1, y1, z1, x2, y2, z2) {
  */
 Util.getEuclideanDistance2 = function(x1, y1, z1, x2, y2, z2) {
   return ((x1 - x2) * (x1 - x2)) +
-    ((y1 - y2) * (y1 - y2)) +
-    ((z1 - z2) * (z1 - z2));
+      ((y1 - y2) * (y1 - y2)) +
+      ((z1 - z2) * (z1 - z2));
 };
 
 /**
