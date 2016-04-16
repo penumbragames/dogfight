57a27cdd48a25afdd531db525ef53b5a75971c89
omgimanerd <alvin.lin.dev@gmail.com>
2016-04-16 17:38:57 -0400

    boiled down threejs extern file

--- extern/threejs.extern.js
+++ extern/threejs.extern.js
@@ -8,31 +8,12 @@
  * @type {Object}
  */
 var THREE = {
-  'Quaternion': function() {},
-  'Vector2': function() {},
   'Vector3': function() {},
-  'Vector4': function() {},
-  'Euler': function() {},
-  'Line3': function() {},
-  'Box2': function() {},
-  'Box3': function() {},
-  'Matrix3': function() {},
-  'Matrix4': function() {},
-  'Ray': function() {},
-  'Sphere': function() {},
-  'Frustum': function() {},
-  'Plane': function() {},
   'Geometry': function() {},
-  'Camera': {
-    'lookAt': function() {}
-  },
-  'CubeCamera': function() {},
-  'OrthographicCamera': function() {},
+  'BoxGeometry': function() {},
   'PerspectiveCamera': function() {},
-  'Material': function() {},
-  'Sprite': function() {},
-  'Particle': function() {},
-  'LensFlare': function() {},
   'Scene': function() {},
   'WebGLRenderer': function() {},
+  'MeshBasicMaterial': function() {},
+  'Mesh': function() {}
 };
