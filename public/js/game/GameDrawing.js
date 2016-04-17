be439a19ac53e6d5299538161b51dcaa78998126
omgimanerd <alvin.lin.dev@gmail.com>
2016-04-16 22:11:56 -0400

    Cleanup

--- public/js/game/GameDrawing.js
+++
@@ -1,8 +0,0 @@
-function GameDrawing(context, scene) {
-  this.context = context;
-  this.scene = scene;
-}
-
-GameDrawing.create = function(context) {
-  return new GameDrawing(context, scene);
-}
--- public/js/game/UIDrawing.js
+++
@@ -1,7 +0,0 @@
-function UIDrawing(context) {
-  this.context = context;
-}
-
-UIDrawing.create = function(context) {
-  return new UIDrawing(context);
-}
